5554c61e79e1db4e976d38cf3e69dd990ecfc3c4
Manuel de la Peña <mdelapenya@gmail.com>
2020-03-02 18:13:09 +0100

    feat: remove stop words from the index

--- indexers/elasticsearch.go
+++ indexers/elasticsearch.go
@@ -22,22 +22,86 @@ var esInstance *es.Client
 type ElasticsearchIndexer struct {
 }
 
+type analysedToken struct {
+	Token       string `json:"token"`
+	StartOffset int    `json:"start_offset"`
+	EndOffset   int    `json:"end_offset"`
+	Word        string `json:"word"`
+	Position    int    `json:"posistion"`
+}
+
+type analysedResponse map[string][]analysedToken
+
 // NewESIndexer returns an Elasticsearch indexer
 func NewESIndexer() Indexer {
 	return &ElasticsearchIndexer{}
 }
 
-// Index indexes an agenda in Elasticsearch
-func (ei *ElasticsearchIndexer) Index(ctx context.Context, event models.AgendaEvent) error {
-	eventJSON, err := event.ToJSON()
+// analyze analyses a text using consino analyser to remove stop words
+func analyze(ctx context.Context, text string) ([]string, error) {
+	esClient, err := getElasticsearchClient()
 	if err != nil {
-		return err
+		return []string{}, err
 	}
 
+	// Set up the APM transaction
+	analyzeTxn := apm.DefaultTracer.StartTransaction("Analyze()", "analyze")
+	// Add current user to the transaction metadata
+	analyzeTxn.Context.SetUsername("cansino")
+	// Store the transaction in a context
+	analyzeTxCtx := apm.ContextWithTransaction(ctx, analyzeTxn)
+	// Mark the transaction as completed
+	defer analyzeTxn.End()
+
+	// Perform the analyze request with the client.
+	analyzeRes, err := esClient.Indices.Analyze(
+		esClient.Indices.Analyze.WithIndex("cansino"),
+		esClient.Indices.Analyze.WithBody(
+			strings.NewReader(getBody(text))),
+		esClient.Indices.Analyze.WithPretty(),
+		esClient.Indices.Analyze.WithContext(analyzeTxCtx),
+	)
+	if err != nil {
+		log.Printf("Error getting analyze response: %s\n", err)
+		return []string{}, err
+	}
+	defer analyzeRes.Body.Close()
+
+	// Deserialize the response into a map.
+	r := analysedResponse{}
+	if err := json.NewDecoder(analyzeRes.Body).Decode(&r); err != nil {
+		// Capture the error
+		apm.CaptureError(analyzeTxCtx, err).Send()
+		log.Printf("Error parsing the analyze response body %v: %s\n", analyzeRes.Body, err)
+		return []string{}, err
+	}
+
+	// Set the response status as transaction result
+	analyzeTxn.Result = analyzeRes.Status()
+
+	tokens := r["tokens"]
+	analysedTokens := make([]string, len(tokens))
+	for i, token := range tokens {
+		analysedTokens[i] = token.Token
+	}
+
+	return analysedTokens, nil
+}
+
+// Index indexes an agenda in Elasticsearch
+func (ei *ElasticsearchIndexer) Index(ctx context.Context, event models.AgendaEvent) error {
 	esClient, err := getElasticsearchClient()
 	if err != nil {
 		return err
 	}
+
+	// extract stop words from the description
+	analysedDescription, err := analyze(ctx, event.Description)
+	event.Description = strings.Join(analysedDescription, " ")
+
+	// extract stop words from the location
+	analysedLocation, err := analyze(ctx, event.Location)
+	event.Location = strings.Join(analysedLocation, " ")
 
 	// Set up the APM transaction
 	txn := apm.DefaultTracer.StartTransaction("Index()", "indexing")
@@ -47,6 +111,11 @@ func (ei *ElasticsearchIndexer) Index(ctx context.Context, event models.AgendaEv
 	txCtx := apm.ContextWithTransaction(ctx, txn)
 	// Mark the transaction as completed
 	defer txn.End()
+
+	eventJSON, err := event.ToJSON()
+	if err != nil {
+		return err
+	}
 
 	// Set up the request object.
 	req := esapi.IndexRequest{
@@ -85,6 +154,10 @@ func (ei *ElasticsearchIndexer) Index(ctx context.Context, event models.AgendaEv
 	}
 
 	return nil
+}
+
+func getBody(text string) string {
+	return `{"analyzer": "spanish_stop", "text": "` + text + `"}`
 }
 
 // getElasticsearchClient returns a client connected to the running elasticseach cluster
